050e24e0204f2df5c195254b711b04af3b4b28cb
Shivam Kumar <csautomaytic@gmail.com>
2025-01-07 10:47:23 +0530

    Update incoming_message_whatsapp_cloud_service.rb

--- app/services/whatsapp/incoming_message_whatsapp_cloud_service.rb
+++ app/services/whatsapp/incoming_message_whatsapp_cloud_service.rb
@@ -10,9 +10,22 @@ class Whatsapp::IncomingMessageWhatsappCloudService < Whatsapp::IncomingMessageB
 
   def download_attachment_file(attachment_payload)
     url_response = HTTParty.get(inbox.channel.media_url(attachment_payload[:id]), headers: inbox.channel.api_headers)
-    File.open("logging_error.txt", 'w') { |file| file.write(url_response) }
-    # This url response will be failure if the access token has expired.
+
+    # Log the response details
+    unless url_response.success?
+      Rails.logger.error(
+        "WhatsApp Media Download Error: \n" \
+        "Status: #{url_response.code}\n" \
+        "Headers: #{url_response.headers}\n" \
+        "Body: #{url_response.body}\n" \
+        "Media ID: #{attachment_payload[:id]}"
+      )
+    end
+
+    # Handle unauthorized response
     inbox.channel.authorization_error! if url_response.unauthorized?
+
+    # Only proceed with download if successful
     Down.download(url_response.parsed_response['url'], headers: inbox.channel.api_headers) if url_response.success?
   end
 end
